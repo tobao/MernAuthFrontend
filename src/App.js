ddc5783e08b57bdd71cc4bfa08289b7f4d52fd6a
To Nguyen Bao <tobao97@gmail.com>
2024-10-28 18:57:30 +0700

    Setting up Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import './Home.scss'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
