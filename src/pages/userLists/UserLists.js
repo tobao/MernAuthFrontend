8c3f953408890ebe64b911e083c2e3e029f0f9ef
To Nguyen Bao <tobao97@gmail.com>
2024-10-29 21:05:03 +0700

    User Stats Component

--- src/pages/userLists/UserLists.js
+++ src/pages/userLists/UserLists.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PageMenu from '../../components/pagemenu/PageMenu'
+import './UserLists.scss'
+import UserStats from '../../components/userStats/UserStats'
 
 const UserLists = () => {
   return (
     <section>
       <div className="container">
         <PageMenu/>
+        <UserStats/>
       </div>
     </section>
   )
