d479bbdf51c8a67a0f4a81026e36b6666609c8ca
To Nguyen Bao <tobao97@gmail.com>
2024-10-30 11:12:56 +0700

    User Table

--- src/pages/userLists/UserLists.js
+++ src/pages/userLists/UserLists.js
@@ -3,6 +3,7 @@ import PageMenu from '../../components/pagemenu/PageMenu'
 import './UserLists.scss'
 import UserStats from '../../components/userStats/UserStats'
 import Search from '../../components/search/Search'
+import { FaTrashAlt } from 'react-icons/fa'
 
 const UserLists = () => {
   return (
@@ -11,14 +12,42 @@ const UserLists = () => {
         <PageMenu/>
         <UserStats/>
         <div className="user-list">
-          <div className="--flex-between">
-            <span>
-              <h3>All Users</h3>
-            </span>
-            <span>
-              <Search/>
-            </span>
+          <div className="table">
+            <div className="--flex-between">
+              <span>
+                <h3>All Users</h3>
+              </span>
+              <span>
+                <Search/>
+              </span>
+            </div>
+            {/* Table */}
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Change Role</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Bao</td>
+                  <td>tobao97@gmail.com</td>
+                  <td>Admin</td>
+                  <td>Change Role</td>
+                  <td>
+                    <span><FaTrashAlt size={20} color="red" /></span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+
         </div>
       </div>
     </section>
