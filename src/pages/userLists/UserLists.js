568d3804090458f9db1bf8d26a925aebdad85054
To Nguyen Bao <tobao97@gmail.com>
2024-10-30 10:46:08 +0700

    User Search Component

--- src/pages/userLists/UserLists.js
+++ src/pages/userLists/UserLists.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PageMenu from '../../components/pagemenu/PageMenu'
 import './UserLists.scss'
 import UserStats from '../../components/userStats/UserStats'
+import Search from '../../components/search/Search'
 
 const UserLists = () => {
   return (
@@ -9,6 +10,16 @@ const UserLists = () => {
       <div className="container">
         <PageMenu/>
         <UserStats/>
+        <div className="user-list">
+          <div className="--flex-between">
+            <span>
+              <h3>All Users</h3>
+            </span>
+            <span>
+              <Search/>
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   )
